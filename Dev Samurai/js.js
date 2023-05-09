075e118fda7562855b703881e08217459727dd36
pachecx <pachecx35@gmail.com>
2023-05-09 10:07:01 -0300

    first commit

--- Dev Samurai/js.js
+++
@@ -1,9 +0,0 @@
-let contator = 1
-
-while(contator <= 10){
-
-    alert(contator)
-    contator++
-}
-
-alert("Fim do programa")
--- Projetos para o portifolio/RocketSeat/Jogo da Advinhação/capturando-valor-do-form-ao-clicar-no-botao/main.js
+++
@@ -1,7 +0,0 @@
-function handleClick(event) {
-  event.preventDefault()
-
-  const inputNumber = document.querySelector('#inputNumber')
-
-  console.log(inputNumber.value)
-}
