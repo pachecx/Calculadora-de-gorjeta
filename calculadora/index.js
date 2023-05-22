fe2802807e6bd1439feb4e57faa03ed93fa9255b
pachecx <pachecx35@gmail.com>
2023-05-22 16:21:54 -0300

    criando a  logica da calculadora

--- calculadora/index.js
+++ calculadora/index.js
@@ -1,8 +1,10 @@
+const btn5 = document.querySelector('.tip5')
+const btn10 = document.querySelector('.tip10')
+const btn15 = document.querySelector('.tip15')
+const btn25 = document.querySelector('.tip25')
+const btn50 = document.querySelector('.tip50')
+const btnCustom = document.querySelector('.tipcustom')
+
 let conta = document.querySelector('#conta')
-let qnt = document.querySelector('#quantidade')
+let pessoa = document.querySelector('#quantidade')
 
-function calcular(){
-    result = Number(conta) / Number(qnt)
-}
-
-alert(result)
