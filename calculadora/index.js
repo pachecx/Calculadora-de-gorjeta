3278cb5a0f50cb805c7388b89626b6f596b64155
pachecx <pachecx35@gmail.com>
2023-05-25 14:49:04 -0300

    inicio da logica

--- calculadora/index.js
+++ calculadora/index.js
@@ -6,13 +6,39 @@ const btn50 = document.querySelector('.tip50')
 const btnCustom = document.querySelector('.tipcustom')
 
 let valor = document.querySelector('#conta')
-let quantiidade = document.querySelector('#quantidade')
+let quantidade = document.querySelector('#quantidade')
 
 const btnCalcular = document.querySelector('.calcular')
 const btnLimpar = document.querySelector('.limpar')
 
+let valorPagar = document.querySelector('.valuetotal')
+
+
+
+function valorTotal(){
+
+    conta = valor.value 
+    pessoa = quantidade.value
+    
+    result = Number(conta) / Number(pessoa)
+
+    valorPagar.textContent = ` $${result}`
+}
+
+function limparValores(){
+    valorPagar.textContent = `$ 0.0`
+}
 
 btnCalcular.addEventListener('click', function(){
     btnCalcular.classList.add('hide')
     btnLimpar.classList.remove('hide')
+
+    valorTotal()
+})
+
+btnLimpar.addEventListener('click', function(){
+    btnLimpar.classList.add('hide')
+    btnCalcular.classList.remove('hide')
+
+    limparValores()
 })
